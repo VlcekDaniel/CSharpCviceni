61c9ad2dd323579281970a688df06a3724cbcddb
VlcekDaniel <VlcekDaaniel@gmail.com>
2021-02-18 09:53:24 +0100

    cv01 adresa

---
+++ Adresa/Adresa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Adresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Josef Novák");
+            Console.WriteLine("Jindrišská 16");
+            Console.WriteLine("111 50, Praha 1");
+          
+            Console.Write("Josef Novák \n");
+            Console.Write("Jindrišská 16 \r");
+            Console.Write("111 50, Praha 1 \n");
+
+            int @if = 10;
+            Console.Write(@$"\\ {@if}");
+
+            Console.ReadKey();
+        }
+    }
+}
