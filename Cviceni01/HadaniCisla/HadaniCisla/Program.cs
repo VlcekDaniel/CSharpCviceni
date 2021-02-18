a494bd14310682c9d73a48b75f87f5f2c36e0113
VlcekDaniel <VlcekDaaniel@gmail.com>
2021-02-18 12:20:58 +0100

    commit hadaniCisel

---
+++ Cviceni01/HadaniCisla/HadaniCisla/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HadaniCisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int cislo = rand.Next(0, 101);
+            int pocetPokusu = 0;
+            do
+            {
+                do
+            {
+                Console.WriteLine("Hadejte cislo: \n");
+                int hadaneCislo =Convert.ToInt32(Console.ReadLine());
+                if (hadaneCislo < cislo) {
+                    Console.WriteLine("vetsi");
+                }
+                if (hadaneCislo > cislo)
+                {
+                    Console.WriteLine("mensi");
+                }
+                if (hadaneCislo == cislo)
+                {
+                    Console.WriteLine("spravne");
+                    break;
+                }
+                pocetPokusu++;
+                if (pocetPokusu == 10) {
+                    Console.WriteLine("prohra");
+                    break;
+                }
+            } while (true);
+                Console.WriteLine("Chcete hrat znovu?(0/1)");
+                int volba = Convert.ToInt32(Console.ReadLine());
+                if (volba == 0) {
+                    break;
+                }
+                cislo = rand.Next(0, 101);
+                pocetPokusu = 0;
+            } while (true);
+        }
+    }
+}
--- Cviceni01/RodneCislo/RodneCislo/Program.cs
+++ Cviceni01/RodneCislo/RodneCislo/Program.cs
@@ -8,7 +8,25 @@ namespace RodneCislo
         {
             String RodneCislo1 = "910703";
             String RodneCislo2 = "586226";
-            Console.WriteLine("Hello World!");
+
+            //vraci true(muz) nebo false(zena)
+            static bool Over(String text) {
+                int znak = Convert.ToInt32(text.Substring(2, 1));
+                if (znak == 0|| znak == 2|| znak == 1 || znak == 3)
+                    return true;
+                return false;
+            }
+            //vraci true(muz) nebo false(zena)
+            static bool OverLong(ulong cislo)
+            {
+                ulong znak = (cislo / 1000)%10;
+                if (znak == 0 || znak == 2|| znak == 1 || znak == 3)
+                    return true;
+                return false;
+            }
+
+            Console.WriteLine(Over(RodneCislo1)?"muz":"zena");
+            Console.WriteLine(OverLong(Convert.ToUInt64(RodneCislo2)) ? "muz" : "zena");
         }
     }
 }
