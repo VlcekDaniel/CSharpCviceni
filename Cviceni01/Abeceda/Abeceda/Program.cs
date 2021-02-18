27eeed8afeab0de1f573c20e5fd905ff63b119e7
VlcekDaniel <VlcekDaaniel@gmail.com>
2021-02-18 10:38:21 +0100

    commit RodneCislo

--- Cviceni01/Abeceda/Abeceda/Program.cs
+++ Cviceni01/Abeceda/Abeceda/Program.cs
@@ -6,7 +6,32 @@ namespace Abeceda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int cislo = 97;
+            for (int i = 0; i < 26; i++)
+            {
+                char a = (char)cislo;
+                Console.Write(a);
+                cislo++;
+            }
+            cislo = 97;
+            Console.WriteLine();
+            while (cislo<=122)
+            {
+                char a = (char)cislo;
+                Console.Write(a);
+                cislo++;
+            }
+            cislo = 97;
+            Console.WriteLine();
+            do
+            {
+                char a = (char)cislo;
+                Console.Write(a);
+                cislo++;
+                if (cislo > 122) {
+                    break;
+                }
+            } while (true);
         }
     }
 }
---
+++ Cviceni01/RodneCislo/RodneCislo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RodneCislo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String RodneCislo1 = "910703";
+            String RodneCislo2 = "586226";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
