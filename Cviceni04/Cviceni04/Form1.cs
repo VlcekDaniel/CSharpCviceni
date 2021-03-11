aa783ef26eb3fc4c96b41553628337d56e46529d
VlcekDaniel <VlcekDaaniel@gmail.com>
2021-03-11 16:31:27 +0100

    commit cv04

---
+++ Cviceni04/Cviceni04/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cviceni04
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+
+        public Form1()
+        {
+            InitializeComponent();
+            stats.UpdatedStats += onUpdateStats;  
+        }
+
+        private void onUpdateStats(object sender, EventArgs eventArgs)
+        {
+            correctLabel.Text = Convert.ToString("Correct:" + stats.Correct);
+            missedLabel.Text = Convert.ToString("Missed: " + stats.Missed);
+            accurancyLabel.Text = Convert.ToString("Accurancy: " + stats.Accurancy);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gameListBox.Items.Add((Keys)random.Next(65,90));
+            gameListBox.Refresh();
+            if (gameListBox.Items.Count > 6) {
+                timer1.Stop();
+                gameListBox.Items.Clear();
+                gameListBox.Items.Add("Game Over!");
+            }
+        }
+
+        private void gameListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool correctKey = false;
+            if (gameListBox.Items.Contains(e.KeyCode))
+            {
+                gameListBox.Items.Remove(e.KeyCode);
+                gameListBox.Refresh();
+                correctKey = true;
+            }
+            else {
+                correctKey = false;
+            }
+
+            if (timer1.Interval > 100)
+            {
+                if (timer1.Interval > 400)
+                {
+                    timer1.Interval -= 60;
+                }
+                else
+                if (timer1.Interval > 250)
+                {
+                    timer1.Interval -= 15;
+                }
+                else
+                if (timer1.Interval > 150)
+                {
+                    timer1.Interval -= 8;
+                }
+                if (timer1.Interval > 800 || timer1.Interval < 90) {
+                    throw new Exception();
+                }
+            }
+            difficultyProgressBar.Value = timer1.Interval;
+            stats.Update((correctKey));
+        }
+    }
+}
---
+++ Cviceni04/Cviceni04/Stats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni04
+{
+    class Stats
+    {
+        public int Correct { get; private set; }
+        public int Missed { get;private set; }
+        public int Accurancy { get;private set; }
+
+        public event UpdatedStatsEventHandler UpdatedStats;
+
+        public delegate void UpdatedStatsEventHandler(object sender, EventArgs e);
+        
+        private void OnUpdatedStats()
+        {
+            UpdatedStatsEventHandler handler = UpdatedStats;
+            if (handler != null)
+                handler(this, new EventArgs());
+        }
+
+        public void Update(bool correctKey) {            
+            if (correctKey)
+            {
+                Correct++;
+            }
+            else {
+                Missed++;
+            }
+            Accurancy = Correct*100 / (Correct + Missed);
+            OnUpdatedStats();
+        }
+    }
+}
